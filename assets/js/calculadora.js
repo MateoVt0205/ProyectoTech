090fe4194530cde5322f02464f805221f22d1b7a
mateoVt0205 <mateovt0205@gmail.com>
2025-02-26 09:33:54 -0500

    Se añaden nuevos cambios al proyecto

--- assets/js/calculadora.js
+++ assets/js/calculadora.js
@@ -1,106 +1,4 @@
-
-// // const userName = document.getElementById('name').value;
-
-// function CalcularConsumo(){
-//     const name = document.getElementById('name').value;
-//     const energia = document.getElementById('energia').value;
-//     //Mirar los valores que ingresa el usuario
-//     console.log(name);
-//     console.log(energia);
-
-//     if(name == "" && energia == ""){
-//         alert("los campos estan vacios, ingrese datos para continuar");}
-
-//         else if(name == "" || energia == ""){
-//             alert("Algunos campos estan vacios, ingrese datos para continuar");
-        
-//     } else {
-
-//         let mensaje = "Este es el consumo de tu energía con los diferentes tipos que tenemos:";
-//         document.getElementById('EnergyType').innerHTML = mensaje;
-//         document.getElementById('card').innerHTML = 
-//         `
-
-// <div class="flex justify-center gap-12">
-
-// <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-//     <svg class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sun-fill" viewBox="0 0 16 16">
-//   <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"/>
-// </svg>    
-//         <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Energia Solar</h5>
-//     <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Hola ${name} Esta es la cantidad de energía que producirías con energía solar al mes</p>
-//     <p class="mb-3 text-gray-500 dark:text-gray-400">${energia}</p>
-// </div>
-
-// <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-//     <svg class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-water" viewBox="0 0 16 16">
-//   <path d="M.036 3.314a.5.5 0 0 1 .65-.278l1.757.703a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.757-.703a.5.5 0 1 1 .372.928l-1.758.703a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0L.314 3.964a.5.5 0 0 1-.278-.65m0 3a.5.5 0 0 1 .65-.278l1.757.703a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.757-.703a.5.5 0 1 1 .372.928l-1.758.703a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0L.314 6.964a.5.5 0 0 1-.278-.65m0 3a.5.5 0 0 1 .65-.278l1.757.703a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.757-.703a.5.5 0 1 1 .372.928l-1.758.703a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0L.314 9.964a.5.5 0 0 1-.278-.65m0 3a.5.5 0 0 1 .65-.278l1.757.703a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.757-.703a.5.5 0 1 1 .372.928l-1.758.703a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.757-.703a.5.5 0 0 1-.278-.65"/>
-// </svg>
-//         <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Energia Hidro Electrica</h5>
-//     <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Hola ${name} Esta es la cantidad de energía que producirías con Energia HidroElectrica al mes</p>
-//     <p class="mb-3 text-gray-500 dark:text-gray-400">${energia}</p>
-// </div>
-
-// <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-// <svg class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wind" viewBox="0 0 16 16">
-//   <path d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5m-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2M0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5"/>
-// </svg>
-//         <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Energia Eolica</h5>
     
-//     <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Hola ${name} Esta es la cantidad de energía que producirías con Energia Eolica al mes</p>
-//     <p class="mb-3 text-gray-500 dark:text-gray-400">${energia}</p>
-// </div>
-
-
-// <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-//     <svg class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" 
-//     viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-globe-americas" viewBox="0 0 16 16">
-//   <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0M2.04 4.326c.325 1.329 2.532 2.54 3.717 3.19.48.263.793.434.743.484q-.121.12-.242.234c-.416.396-.787.749-.758 1.266.035.634.618.824 1.214 1.017.577.188 1.168.38 1.286.983.082.417-.075.988-.22 1.52-.215.782-.406 1.48.22 1.48 1.5-.5 3.798-3.186 4-5 .138-1.243-2-2-3.5-2.5-.478-.16-.755.081-.99.284-.172.15-.322.279-.51.216-.445-.148-2.5-2-1.5-2.5.78-.39.952-.171 1.227.182.078.099.163.208.273.318.609.304.662-.132.723-.633.039-.322.081-.671.277-.867.434-.434 1.265-.791 2.028-1.12.712-.306 1.365-.587 1.579-.88A7 7 0 1 1 2.04 4.327Z"/>
-// </svg>
-//         <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
-//     </svg>
-//         <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Energia Geotermica</h5>
-//     <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Hola ${name} Esta es la cantidad de energía que producirías con Energia Geotermica al mes</p>
-//     <p class="mb-3 text-gray-500 dark:text-gray-400">${energia}</p>
-// </div>
-// </div>
-// ` 
-// //cambiar los valores de las demas energias usando sus variables 
-//     }
-    
-// }
-// document.getElementById('calcular').addEventListener('click', CalcularConsumo);
-
-
-// // Parametros de cada fuente
-
-// let solar = 135, n_solar = 0.17;
-// let eolica = 284, n_eolica = 0.4;
-// let hidroElecrtica = 600, n_hidroElectrica = 0.17;
-// let geotermica = 504, n_geotermica = 0.9;
-
-// // Formulas
-
-// let fSolar = (E / solar * n_solar);
-// let fEolica = (E / eolica * n_eolica);
-// let fHidroElectrica = (E /600 * 0.17)
-// let fGeotermica = (E / 504 * 0.9)
-
-// const userName = document.getElementById('name').value;
-// Recoger energia + condiciones
-
-// function ConsumoCalculado(){
-    //     let E = parseFloat(document.getElementById("energia").value);
-    
-    //         if(isNaN (E) && E <= 0 ){
-        //             alert("Por favor ingrese un consumo en kWh valido");
-        //         }
-        //     }
-        // Formulas (E = ENERGIA QUE METE EL USUARIO)
-        
-        // Parametros de cada fuente
-        // n = Eficiencia energetica de energia
-        
         
         function CalcularConsumo(){
             const name = document.getElementById('name').value;
@@ -124,7 +22,7 @@
         let fGeotermica = (E / geotermica * n_geotermica).toFixed(4);
 
         if(isNaN(E) || E <= 0){
-            alert("Verifique que los campos ingresados sean validos. (Energía mayor a 0 en kWh")
+            alert("Verifique que los campos ingresados sean validos. (Energía mayor a 0 en kWh)")
         }
            else if(name == ""){
         alert("Algunos campos estan vacios, ingrese datos para continuar");}
